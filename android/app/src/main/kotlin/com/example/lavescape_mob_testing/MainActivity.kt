ad3f1f1e57d3c32e6a63ae0ddff3ae9cc1e32a84
Zyad Mostafa <zyadmo026@gmail.com>
2025-10-20 21:01:10 +0300

    Add initial Flutter project setup

---
+++ android/app/src/main/kotlin/com/example/lavescape_mob_testing/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lavescape_mob_testing
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/screen/sign_up_with_phone_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SignUpWithPhoneScreen extends StatelessWidget {
+  const SignUpWithPhoneScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Sign Up with Phone')),
+      body: const Center(child: Text('Phone Sign-Up Screen Content Here')),
+    );
+  }
+}
